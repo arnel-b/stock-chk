91fadc8aa3d557b8c02834e10c47356dd3a0a6b2
Arnel <arnel.b@solidliftparts.com>
2022-09-23 18:59:35 +0800

    added mention in slack text

--- kinsey/stock_checker.py
+++ kinsey/stock_checker.py
@@ -167,7 +167,7 @@ class Scraper(object):
             headers = {
                 'Content-Type': 'application/x-www-form-urlencoded',
             }
-            data = 'payload={"username": "Kinseys Stock Check", "color":"#D00000", "pretext": "Download Complete", "text": "' + dl_link + '"}'
+            data = 'payload={"username": "Kinseys Stock Check", "color":"#D00000", "pretext": "<@U8RR4NQT1>, <@UC5MLB7DY> Download Complete", "text": "' + dl_link + '"}'
             slack = requests.post('https://hooks.slack.com/services/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', headers=headers, data=data)
             print(slack)
             
